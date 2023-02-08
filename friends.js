4207dc7db9693dac3df4e91cc2c4b4dceffc5c02
Avery Berryman <averyberryman@gmail.com>
2023-02-08 14:58:14 -0700

    Add part 2

--- friends.js
+++ friends.js
@@ -1,5 +1,10 @@
-var bestFriend = "Jeff";
+var bestFriend = "Sandra";
+var otherFriend = "Carson";
 
 if (bestFriend === "Jeff") {
     console.log(`${bestFriend}, you are my best friend.`);
+} else if (otherFriend === "Carson") {
+    console.log("you're my other best friend.");
 }
+
+
